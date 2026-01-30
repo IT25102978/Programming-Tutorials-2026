0d6a03dab1efeb790ef48ee6641aaa5744699741
IT25102978 <it25102978@my.sliit.lk>
2026-01-30 11:57:55 +0530

    Add files via upload
    Programming-Tutorials - Question05

---
+++ Questions05.java
@@ -0,0 +1,24 @@
+public class Question05 
+{
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        
+        int exp1 = square(add(multiply(3, 4), multiply(5, 7)));
+        System.out.println("Result of (3 * 4 + 5 * 7)^2 = " + exp1);
+
+        int exp2 = add(square(add(4, 7)), square(add(8, 3)));
+        System.out.println("Result of (4 + 7)^2 + (8 + 3)^2 = " + exp2);
+    }
+}
